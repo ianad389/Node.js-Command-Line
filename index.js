c59b36a613590da5c703c57384267ba30a897bf9
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2022-10-09 12:29:40 -0700

    almost done with index.js questions

--- index.js
+++ index.js
@@ -1,3 +1,178 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./template');
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the manager?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the office number?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is the manager email?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: 'What is the manager id?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+]
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the egineer?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the github profile?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is the egineer email?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: 'What is the egineer id?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+]
+const internquestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school   ',
+        message: 'What is the office number?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is the manager email?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: 'What is the manager id?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+]
+
+
+function init() {
+    inquirer.prompt(managerQuestions)
+        .then(function(answer) {
+            console.log(answer);
+  
+        });
+}
+
+// Function call to initialize app
+init();
